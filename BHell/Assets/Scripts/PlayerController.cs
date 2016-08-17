6f6ab9277e5ca4029ade9939e44570af6914a99d
Ezra Ablaza <ezrabl42@gmail.com>
2016-08-17 04:25:01 -0400

    Assets and placeholders for a regular 2d Arcade shooter.

---
+++ BHell/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController: MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 10.0f; //starts out at 10
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() //using physics for moving
+    {
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.velocity = movement * speed;
+        //no Newtonian forces, just moving U/D/L/R
+
+
+    }
+}
