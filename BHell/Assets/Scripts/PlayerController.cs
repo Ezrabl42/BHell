b212516c52115f4e0ee8da2c689868bc9720821b
Ezra Ablaza <ezrabl42@gmail.com>
2016-08-18 04:41:24 -0400

    Fixed blender file for orientation upon loading.
    **Important: Unity pitches blend objects forward by 90 degrees because
    the Y axis is considered up. Accomodate this in Blender by selecting Object
    Mode, then changing the Euler X Axis to -90.  Reset the coordinates by
    hitting Ctrl+A and then selecting Rotation. The Euler axis should all be 0
    degrees.  Set the Euler X Axis to 90, so visually your model looks exactly
    the same as before.  This trick will allow Unity to "rotate" your object
    to the preferred orientation when you add the model into the project.
    
    Also introduced banking tilt using Quaternions
    Learning Quaternions will be useful for later objects

--- BHell/Assets/Scripts/PlayerController.cs
+++ BHell/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController: MonoBehaviour
+[System.Serializable]
+public class Boundary
 {
+    public float xMin, xMax, zMin, zMax;
 
-    private Rigidbody rb;
+}
 
-    public float speed;
+public class PlayerController: MonoBehaviour // ':' means 'inheriting from'
+{
+    public Boundary bounds; //use the class we defined here!
 
+    private Rigidbody rb; //we'll use the rigid body for the velocity
+    private Quaternion endRotation; //Store target orientation for banking here!
+
+    public float speed; //edit these in the inspector
+    public float tilt; 
+  
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        speed = 10.0f; //starts out at 10
+        rb = GetComponent<Rigidbody>();   
     }
 
     void Update()
     {
-
     }
 
     void FixedUpdate() //using physics for moving
-    {
+    { 
+        float moveHorizontal = Input.GetAxis("Horizontal"); //these two will be floats between 0 and 1
+        float moveVertical = Input.GetAxis("Vertical"); //thus the in-game speed will be dictated by map size
 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);  //the vector that points in the direction in which we wish to move
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.velocity = movement * speed;
+        rb.velocity = movement * speed; //a speed of around 100 will do for a map this size
         //no Newtonian forces, just moving U/D/L/R
 
-
+        rb.position = new Vector3 //We have to stay within the box, so we keep the position within the set bounds
+        (
+            Mathf.Clamp(rb.position.x, bounds.xMin, bounds.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, bounds.zMin, bounds.zMax)
+        ); 
+        
+        endRotation = Quaternion.AngleAxis(-tilt * (rb.velocity.x)/100f, transform.forward);  //we scale down the speed to be between 0 and 1
+        //the tilt can be expressed as degrees.  tilt is inverted because transformations are by default clockwise
+        rb.rotation = endRotation;
     }
 }
